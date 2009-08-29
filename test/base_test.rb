9f60032689a46e4c5884561b27a68fc09435df94
Fabien Jakimowicz <fabien@jakimowicz.com>
2009-08-29 09:44:17 +0200

    attr_reader is a private method, we have to use the 'send' hack to access it

--- test/base_test.rb
+++ test/base_test.rb
@@ -2,10 +2,9 @@ require "test_helper"
 
 class BaseTest < Test::Unit::TestCase
   should "store raw data from plist at initialization" do
-    AppStore::Base.attr_reader :raw
+    AppStore::Base.send :attr_reader, :raw
     base = AppStore::Base.new :plist => "plist instance"
     assert_equal "plist instance", base.raw
-  end
-  
+  end  
   
 end
