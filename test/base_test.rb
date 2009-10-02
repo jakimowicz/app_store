fad38dbf4b049139411bd38a7471f229a5d7dbb0
Fabien Jakimowicz <fabien@jakimowicz.com>
2009-10-02 21:03:25 +0200

    update BaseTest according to previous modification on Base class (:raw property added)

--- test/base_test.rb
+++ test/base_test.rb
@@ -2,7 +2,6 @@ require "test_helper"
 
 class BaseTest < Test::Unit::TestCase
   should "store raw data from plist at initialization" do
-    AppStore::Dummy.send :attr_reader, :raw
     base = AppStore::Dummy.new :plist => "plist instance"
     assert_equal "plist instance", base.raw
   end
